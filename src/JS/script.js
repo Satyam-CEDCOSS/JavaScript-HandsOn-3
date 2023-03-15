3817a793171f2af2a13a1ff7c49cb6bccd90b654
satyam01032023 <satyambajpai@cedcoss.com>
2023-03-15 10:46:49 +0530

    20th question

--- src/JS/script.js
+++ src/JS/script.js
@@ -105,3 +105,197 @@ function testLogicalOr(val) {
   }
   testLogicalOr(15);
 
+// 71 Introducing Else Statements
+function testElse(val) {
+    let result = "";
+    // Only change code below this line
+    if (val > 5) {
+      result = "Bigger than 5";
+    }
+    else{
+      result = "5 or Smaller";
+    }
+    // Only change code above this line
+    return result;
+  }
+  testElse(4);
+
+// 72 Introducing Else If Statements
+function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    }
+    else if (val < 5) {
+      return "Smaller than 5";
+    }
+    else{
+      return "Between 5 and 10";
+    }
+  }
+  testElseIf(7);
+
+// 73 Logical Order in If Else Statements
+function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+  }
+  orderMyLogic(7);
+
+// 74 Changing IF Else Statements
+function testSize(num) {
+    // Only change code below this line
+    if (num<5){
+      return "Tiny"
+    }
+    else if (num<10){
+      return "Small"
+    }
+    else if (num<15){
+      return "Medium"
+    }
+    else if (num<20){
+      return "Large"
+    }
+    else{
+      return "Huge";
+    }
+    // Only change code above this line
+  }
+  testSize(7);
+
+// 75 Golf Code
+function golfScore(par, strokes) {
+    // Only change code below this line
+    if (strokes == 1){
+      return "Hole-in-one!"
+    }
+    else if (strokes == 2){
+      return "Eagle"
+    }
+    else if (strokes == 3){
+      return "Birdie"
+    }
+    else if (strokes-par==0){
+      return "Par"
+    }
+    else if (strokes-par==1){
+      return "Bogey"
+    }
+    else if (strokes-par==2){
+      return "Double Bogey"
+    }
+    else{
+      return "Go Home!"
+    }
+    return "Change Me";
+    // Only change code above this line
+  }
+  golfScore(5, 4);
+
+// 76 Selecting from Many Options with Switch Statements
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+      case 1:
+        answer = "alpha";
+        break;
+      case 2:
+        answer = "beta";
+        break;
+      case 3:
+        answer = "gamma";
+        break;
+      case 4:
+        answer = "delta";
+        break;
+    }
+    // Only change code above this line
+    return answer;
+  }
+  // Change this value to test
+  caseInSwitch(1);
+
+// 77 Adding a Default Option in Switch Statements
+function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val){
+      case "a":
+        answer = "apple";
+        break;
+      case "b":
+        answer = "bird";
+        break;
+      case "c":
+        answer = "cat";
+        break;
+      default:
+        answer = "stuff";
+    }
+    // Only change code above this line
+    return answer;
+  }
+  switchOfStuff(1);
+
+// 78 Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val){
+      case 1:
+      case 2:
+      case 3:
+        answer = "Low"
+        break;
+      case 4:
+      case 5:
+      case 6:
+        answer = "Mid"
+        break;
+      case 7:
+      case 8:
+      case 9:
+        answer = "High"
+    }
+    // Only change code above this line
+    return answer;
+  }
+  sequentialSizes(1);
+
+// 79 Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val){
+      case "bob":
+        answer = "Marley"
+        break;
+      case 42:
+        answer = "The Answer"
+        break;
+      case 1:
+        answer = "There is no #1"
+        break;
+      case 99:
+        answer = "Missed me by this much!"
+        break;
+      case 7:
+        answer = "Ate Nine"
+    }
+    return answer;
+  }
+  chainToSwitch(7);
+
+// 80 Returning Boolean Values from Functions
+function isLess(a, b) {
+    // Only change code below this line
+    return(b-a>0)
+    // Only change code above this line
+  }
+  isLess(10, 15);
