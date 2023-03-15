2dfc4aacd6b56a9aa39688f3e49962efbc935ca8
satyam01032023 <satyambajpai@cedcoss.com>
2023-03-15 11:32:33 +0530

    30th question

--- src/JS/script.js
+++ src/JS/script.js
@@ -299,3 +299,139 @@ function isLess(a, b) {
     // Only change code above this line
   }
   isLess(10, 15);
+
+// 81 Return Early Pattern for Functions
+// Setup
+function abTest(a, b) {
+    // Only change code below this line
+    if(a<0 || b<0){
+      return undefined
+    }
+    else{
+      return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+    }
+    // Only change code above this line
+  }
+  abTest(2,2);
+
+// 82 Counting Cards
+let count = 0;
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+  // Only change code above this line
+}
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+// 83 Build JavaScript Objects
+const myDog = {
+    // Only change code below this line
+    "name": "Bruno",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["anuj","ayush"]
+    // Only change code above this line
+  };
+
+// 84 Accessing Object Properties with Dot Notation
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  // Only change code below this line
+  const hatValue = testObj.hat;      // Change this line
+  const shirtValue = testObj.shirt;    // Change this line
+
+// 85 Accessing Object Properties with Bracket Notation
+const testObj1 = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  // Only change code below this line
+  const entreeValue = testObj1["an entree"];
+  const drinkValue = testObj1["the drink"];
+
+// 86 Accessing Object Properties with Variables
+// Setup
+const testObj2 = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  // Only change code below this line
+  const playerNumber = 16;  // Change this line
+  const player = testObj2[playerNumber];   // Change this line
+
+// 87 Updating Object Properties
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  // Only change code below this line
+  myDog.name = "Happy Coder"
+
+// 88 Add New Properties to a JavaScript Object
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  myDog.bark = "bow-wow";
+
+// 89 Delete Properties from a JavaScript Object
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  // Only change code below this line
+  delete myDog.tails
+
+// 90 Using Objects for Lookup
+// Setup
+function phoneticLookup(val) {
+    let result = "";
+  
+    // Only change code below this line
+  
+    var value = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+    };
+    result = value[val]
+    // Only change code above this line
+    return result;
+  }
+  phoneticLookup("charlie");
